ed2082c1fca218d5fa9d6f840c93c5bcf2b0e309
Liam Mitchell <git@liam.geek.nz>
2018-03-17 23:44:13 +0100

    Bump webpack

--- webpack.config.js
+++ webpack.config.js
@@ -3,59 +3,54 @@ const path = require('path')
 const webpack = require('webpack')
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
 
-module.exports = ({prod} = {}) => ({
-  entry: [
-    'babel-polyfill',
-    'app'
-  ],
-  target: 'web',
-  resolve: {
-    // https://github.com/babel/babel-loader/issues/149#issuecomment-278462040
-    symlinks: false,
-    modules: [
-      path.join(__dirname, 'modules'),
-      'node_modules'
-    ],
-    extensions: ['.js', '.json']
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js?$/,
-        use: {
-          loader: 'babel-loader',
-        }
-      }
-    ]
-  },
-  output: {
-    path: path.join(__dirname, 'build'),
-    publicPath: '/',
-    filename: 'app.js',
-  },
-  devtool: !prod && 'eval-source-map',
-  devServer: {
-    contentBase: path.join(__dirname, 'public'),
-    watchContentBase: true,
-    historyApiFallback: {
-      disableDotRule: true,
+module.exports = ({prod} = {}) => {
+  return {
+    mode: prod ? 'production' : 'development',
+    entry: ['babel-polyfill', 'app'],
+    target: 'web',
+    resolve: {
+      // https://github.com/babel/babel-loader/issues/149#issuecomment-278462040
+      symlinks: false,
+      modules: [path.join(__dirname, 'modules'), 'node_modules'],
+      extensions: ['.js', '.json'],
     },
-    hot: true,
-    compress: true,
-    port: 8080,
-    stats: 'errors-only',
-  },
-  plugins: [
-    !prod && new webpack.HotModuleReplacementPlugin(),
-    prod && new webpack.DefinePlugin({
-      'process.env.NODE_ENV': JSON.stringify('production'),
-    }),
-    prod && new UglifyJsPlugin({
-      uglifyOptions: {
-        output: {
-          semicolons: false, // Use newlines instead of semicolons to reduce git change size.
+    module: {
+      rules: [
+        {
+          test: /\.js?$/,
+          use: {
+            loader: 'babel-loader',
+          },
         },
+      ],
+    },
+    output: {
+      path: path.join(__dirname, 'build'),
+      publicPath: '/',
+      filename: 'app.js',
+    },
+    devServer: {
+      contentBase: path.join(__dirname, 'public'),
+      watchContentBase: true,
+      historyApiFallback: {
+        disableDotRule: true,
       },
-    }),
-  ].filter(Boolean),
-})
+      hot: true,
+      compress: true,
+      port: 8080,
+      stats: 'errors-only',
+    },
+    optimization: {
+      minimizer: [
+        new UglifyJsPlugin({
+          uglifyOptions: {
+            output: {
+              semicolons: false, // Use newlines instead of semicolons to reduce git change size.
+            },
+          },
+        }),
+      ],
+    },
+    plugins: [!prod && new webpack.HotModuleReplacementPlugin()],
+  }
+}
